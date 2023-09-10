bdd16eda06bd3c1a8d80dcac9ebc1c9addc89dcd
thi-le <thi-le@student.42.fr>
2023-09-10 20:03:48 +0200

    complie ok

---
+++ includes/parsing.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thi-le <thi-le@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 18:35:49 by thi-le            #+#    #+#             */
+/*   Updated: 2023/09/10 19:21:30 by thi-le           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+
+#include "structs.h"
+#include "utils.h"
+#include "libft.h"
+
+# define ERROR_NUM_ARGS "Parse error: Invalid number of arguments\n"
+# define ERROR_FILE "Parse error: file cannot be opened\n"
+# define ERR_READ_FILE "Parse error: cannot read the file\n"
+# define ERR_INFOS_ELEM "Error: infos are not correct\n"
+
+//PARSING
+int parsing(t_scene *sc, t_objs *obj, int ac, char **av);
+int init_and_parse(t_scene *sc, t_objs *objs, char **av);
+int parse_input(int ac, char **av);
+int check_line(t_scene *sc, t_objs *objs, char *line);
+int first_element(char *elem);
+int add_to_struct(t_scene *sc, t_objs *obj, int type, char *line);
+int add_ambient(t_scene *sc, char **elems);
+int add_light(t_scene *sc, char **elems);
+int add_camera(t_scene *sc, char **elems);
+int add_a_sphere(t_objs **obj, char **elems);
+int add_a_cylindre(t_objs *obj, char **elems);
+int add_a_plan(t_objs *obj, char **elems);
+t_vector get_position(char *elems);
+t_color get_color(char *elems);
+int check_position(char *elems);
+int check_color(char *elems);
+void init_scene(t_scene *sc);
+void init_sc_camera(t_scene *sc);
+void init_sc_light(t_scene *sc);
+void init_sc_ambient(t_scene *sc);
+
+//DEBUG
+void print_struct(t_scene *sc, t_objs *obj);
+
+
+#endif 
--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,60 @@
 /*   By: thi-le <thi-le@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:08:38 by thi-le            #+#    #+#             */
-/*   Updated: 2023/09/09 18:58:30 by thi-le           ###   ########.fr       */
+/*   Updated: 2023/09/10 20:02:38 by thi-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "miniRT.h"
 
+int	print_error(char *str, int state)
+{
+	if (str)
+		ft_putendl(str);
+	return (state);
+}
+
+int	init_display(t_data *data, char *fname)
+{
+	data->mlx_ptr = mlx_init();
+	if (!data->mlx_ptr)
+		return (print_error("Error: mlx_init failed", 1));
+	data->win_ptr = mlx_new_window(data->mlx_ptr, W_WIDTH, W_HEIGHT, fname);
+	if (!data->win_ptr)
+		return (
+		print_error("Error: mlx_new_window failed", 2));
+	data->img_ptr = mlx_new_image(data->mlx_ptr,W_WIDTH, W_HEIGHT);
+	if (!data->img_ptr)
+		return (
+		print_error("Error: mlx_new_image failed", 3));
+	data->addr = mlx_get_data_addr(data->img_ptr, &data->bits_per_pixel, \
+	&data->line_length, &data->endian);
+	if (!data->addr)
+		return (
+		print_error("Error: mlx_get_data_addr failed", 4));
+	return (SUCESS);
+}
 
 int main(int ac, char **av)
 {
-    t_scene sc;
-    t_objs obj;
+	t_scene	sc;
+	t_objs	obj;
+	t_data	data;
+	//int		state;
 
-    if(parsing(&sc, &obj, ac, av))
-        return(1);
+	if (ac != 2)
+		return (ft_print_help());
+	if (parsing(&sc, &obj, ac, av) != SUCESS)
+		return (1);
+	data.scene = &sc;
+	data.objs = &obj;
+	// state = init_display(&data, av[1]);
+	// if (state != SUCESS)
+	// 	return (clean(&data, state));
+	// state = draw(&data);
+	// if (state != SUCESS)
+	// 	return (clean(&data, state));
+	// hookloop(&data);
+	
     return (0);
 }
